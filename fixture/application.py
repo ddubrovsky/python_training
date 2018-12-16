ba8c4c32750b9d97033f8c22e0798d5b6f476615
Dmitrii <kddima1976@gmail.com>
2018-12-16 21:22:35 +0400

    Isolated class to control the sessions

--- fixture/application.py
+++ fixture/application.py
@@ -1,5 +1,6 @@
 __autor__ = 'Dmitrii Dubrovskii'
 from selenium import webdriver
+from fixture.session import SessionHelper
 
 
 class Application:
@@ -7,17 +8,11 @@ class Application:
     def __init__(self):
         self.wd = webdriver.Firefox()
         self.wd.implicitly_wait(30)
+        self.session = SessionHelper(self)
 
     def open_home_page(self):
         wd = self.wd
         wd.get("http://localhost/addressbook/")
-
-    def login(self, username, password):
-        wd = self.wd
-        self.open_home_page()
-        wd.find_element_by_name("user").send_keys(username)
-        wd.find_element_by_name("pass").send_keys(password)
-        wd.find_element_by_xpath("//input[@value='Login']").click()
 
     def open_groups_page(self):
         wd = self.wd
@@ -39,10 +34,6 @@ class Application:
     def return_to_group_page(self):
         wd = self.wd
         wd.find_element_by_link_text("group page").click()
-
-    def logout(self):
-        wd = self.wd
-        wd.find_element_by_link_text("Logout").click()
 
     def destroy(self):
         self.wd.quit()
