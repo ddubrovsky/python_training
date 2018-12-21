3a0b8cc3f7523a484b0242d5768294592df484b9
ddubrovsky <kddima1976@gmail.com>
2018-12-21 09:33:29 +0400

    Реализована проверка валидности фикстуры и проверка состояния сессии (тест group)

--- test/test_del_group.py
+++ test/test_del_group.py
@@ -2,6 +2,4 @@ __autor__ = 'Dmitrii Dubrovskii'
 
 
 def test_delete_first_group(app):
-    app.session.login(username="admin", password="secret")
     app.group.delete_first_group()
-    app.session.logout()
