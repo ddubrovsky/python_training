90b9742368d581b248dc5bfe8898aca874efb849
Dmitrii <kddima1976@gmail.com>
2018-12-22 22:26:02 +0400

    Добавленно выполнение предусловий для модификаций

--- test/test_modify_contact.py
+++ test/test_modify_contact.py
@@ -4,7 +4,11 @@ from model.contact import Contact
 
 
 def test_add_contact_fname(app):
+    if app.contact.count() == 0:
+        app.contact.create(Contact(fname="test"))
     app.contact.modify_first_contact(Contact(fname="Stich"))
 
 def test_contact_notes(app):
+    if app.contact.count() == 0:
+        app.contact.create(Contact(fname="test"))
     app.contact.modify_first_contact(Contact(notes="Some notes"))
--- test/test_modify_group.py
+++ test/test_modify_group.py
@@ -4,7 +4,11 @@ from model.group import Group
 
 
 def test_add_group_name(app):
+    if app.group.count() == 0:
+        app.group.create(Group(name="test"))
     app.group.modify_first_group(Group(name="New group"))
 
 def test_group_header(app):
+    if app.group.count() == 0:
+        app.group.create(Group(name="test"))
     app.group.modify_first_group(Group(header="New header"))
