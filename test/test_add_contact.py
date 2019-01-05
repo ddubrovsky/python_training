7cef5d6cb91f4042859c1782b6ed64425c751527
Dmitrii <kddima1976@gmail.com>
2019-01-05 19:57:43 +0400

    Хеширование групп и контактов(Ускорение массовых операций)

--- test/test_add_contact.py
+++ test/test_add_contact.py
@@ -9,19 +9,19 @@ def test_add_contact(app):
                            company="Hollywood", address="Los Angeles, USA", sellphone="+1234567890", email="arni@hollywood.com",
                            bday="30", bmonth="July", byear="1947", address2="mansion near LA", notes="bla-bla-bla")
     app.contact.create(contact)
+    assert len(old_contacts)+1 == app.contact.count()
     new_contacts = app.contact.get_contact_list()
-    assert len(old_contacts)+1 == len(new_contacts)
     old_contacts.append(contact)
     assert sorted(old_contacts, key=Contact.contact_id_or_max) == sorted(new_contacts, key=Contact.contact_id_or_max)
 
 
-def test_add_empty_contact(app):
-    old_contacts = app.contact.get_contact_list()
-    contact = Contact(fname="", lname="", nick="", title="",
-                           company="", address="", sellphone="", email="",
-                           bday="-", bmonth="-", byear="-", address2="", notes="")
-    app.contact.create(contact)
-    new_contacts = app.contact.get_contact_list()
-    assert len(old_contacts)+1 == len(new_contacts)
-    old_contacts.append(contact)
-    assert sorted(old_contacts, key=Contact.contact_id_or_max) == sorted(new_contacts, key=Contact.contact_id_or_max)
+#def test_add_empty_contact(app):
+#    old_contacts = app.contact.get_contact_list()
+#    contact = Contact(fname="", lname="", nick="", title="",
+#                           company="", address="", sellphone="", email="",
+#                           bday="-", bmonth="-", byear="-", address2="", notes="")
+#    app.contact.create(contact)
+#    new_contacts = app.contact.get_contact_list()
+#    assert len(old_contacts)+1 == len(new_contacts)
+#    old_contacts.append(contact)
+#    assert sorted(old_contacts, key=Contact.contact_id_or_max) == sorted(new_contacts, key=Contact.contact_id_or_max)
