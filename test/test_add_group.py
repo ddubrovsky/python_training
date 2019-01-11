b2c4f1ae9e08f7b8014138f44c931dae25324b72
ddubrovsky <kddima1976@gmail.com>
2019-01-11 19:30:36 +0400

    Отделение тестовых данных от сценариев

--- test/test_add_group.py
+++ test/test_add_group.py
@@ -2,25 +2,8 @@
 __autor__ = 'Dmitrii Dubrovskii'
 from model.group import Group
 import pytest
-import random
-import string
+from data.add_group import constant as testdata
 
-
-def random_string(prefix, maxlen):
-    symbols = string.ascii_letters + string.digits + string.punctuation + " "*10
-    return prefix + "".join([random.choice(symbols) for i in range(random.randrange(maxlen))])
-    #генератор случайных строк
-
-testdata = [Group(name="", header="", footer="")] + [
-    #+5 контактов со случайными значениями
-    Group(name=random_string('name', 10), header=random_string('header', 20), footer=random_string('footer', 20))
-    for i in range (5)
-    # или 3 параметра пробегают по двум возможным значениям(пусто или случайное)
-    #Group(name=name, header=header, footer=footer)
-    #for name in ["", random_string('name', 10)]
-    #for header in ["", random_string('header', 20)]
-    #for footer in ["", random_string('footer', 20)]
-]
 
 @pytest.mark.parametrize('group', testdata, ids=[repr(x) for x in testdata])
 def test_add_group(app, group):
