cfabf5db2e1b8a5f6c7d66991823720c50ce365e
ddubrovsky <kddima1976@gmail.com>
2019-02-06 15:22:19 +0400

    Добавлена проверка email и phones для задания 21

--- test/test_check_random_contact.py
+++ test/test_check_random_contact.py
@@ -3,7 +3,7 @@ import re
 from model.contact import Contact
 
 
-def test_contact_on_home_page_wit_db(app, db):
+def test_contact_on_home_page_with_db(app, db):
     contact_from_home_page = sorted(app.contact.get_contact_list(), key=Contact.contact_id_or_max)
     contact_from_db = sorted(db.get_contact_list(), key=Contact.contact_id_or_max)
     assert len(contact_from_home_page) == len(contact_from_db)
@@ -13,8 +13,8 @@ def test_contact_on_home_page_wit_db(app, db):
         assert contact_from_home_page[i].lname == contact_from_db[i].lname
         assert contact_from_home_page[i].fname == contact_from_db[i].fname
         assert contact_from_home_page[i].address == contact_from_db[i].address
-        #assert contact_from_home_page[i].all_email_from_home_page == filter_email(contact_from_db[i])
-        #assert contact_from_home_page[i].all_phones_from_home_page == merge_phones_like_on_page(contact_from_db[i])
+        assert contact_from_home_page[i].all_email_from_home_page == contact_from_db[i].all_email_from_home_page
+        assert contact_from_home_page[i].all_phones_from_home_page == contact_from_db[i].all_phones_from_home_page
 
 
 '''
