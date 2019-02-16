f2527a517a6d46b169d145b3234f41566f74bb67
Dmitrii <kddima1976@gmail.com>
2019-02-16 19:03:53 +0400

    Behavior Driven Testing (BDD)

---
+++ bdd/group_scenarios.py
@@ -0,0 +1,13 @@
+__author__='Dmitrii Dubrovskii'
+
+from pytest_bdd import scenario
+from .group_steps import *
+
+@scenario('groups.feature', 'Add new group')
+def test_add_new_group():
+    pass
+
+
+@scenario('groups.feature', 'Delete group')
+def test_delete_group():
+    pass
